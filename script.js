60d2355171393704218d992e6a3c3ce3a814fa21
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-04-06 17:52:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-function insert_Row() {
-    //Write your code here
-  
-  
+function insert_Row(){
+	let table = document.getElementById("sampleTable");
+	let newRowHTML = document.getElementById("newRow").innerHTML;
+
+	let temp = document.createElement("tbody");
+	temp.innerHTML= newRowHTML;
+
+	let newRow= temp.firstElementChild;
+
+	table.insertBefore(newRow, table.firstElementChild);
 }
