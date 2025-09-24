555ac9138dd590676109beb17c25891ea0c29273
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-09-25 00:07:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function insert_Row() {
-    let table = document.getElementById("sampleTable");
+    let table = document.getElementById("table");
 	  table.innerHTML += (`
 		  <tr>
 			  <td>New Cell1</td>
 			  <td>New Cell2</td>
 		  </tr>
 	  `)
-  
+
 }
